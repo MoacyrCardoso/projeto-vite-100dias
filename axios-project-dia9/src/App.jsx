f52aca795db11a4a98eb91b422b693b2f79a7e2f
Moacyr Cardoso <moacircardoso2012@gmail.com>
2024-12-31 18:00:22 -0300

    secund commit

---
+++ axios-project-dia9/src/App.jsx
@@ -0,0 +1,34 @@
+import pokemonInstance from './helper/axios-instance'
+import {useState ,useEffect} from 'react'
+import "./App.css"
+import useAxios from './hook/use-axios'
+
+function App(){
+  const [pokemonList, loading, error] = useAxios({
+    axiosInstance: pokemonInstance,
+    method: 'GET',
+    url: 'pokemon',
+    })
+  console.log(pokemonList, loading, error)
+
+  if (loading){
+    return <div>Loading...</div>
+  }
+  if (error){
+    return <div>Error: {error}</div>
+  }
+
+  return (
+    <>
+       <div className="App">
+            Front Beginners
+          <div>{pokemonList.results.map((pokemon, index)=>
+                (<div key={index}>{pokemon.name}</div>))}
+          </div>
+       </div>
+    </>
+  
+  )
+}
+
+export default App
